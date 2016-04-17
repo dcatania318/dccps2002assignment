21c2338fba97c3db0aa25b7b349cd133a5ce5ed8
Daniel Catania <dcatania318@gmail.com>
2016-04-17 22:10:19 +0200

    removed everything to reconfigure

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Aw, Dinja");
-        System.out.println("Connor calling");
-        System.out.println("Over and out");
-    }
-}
