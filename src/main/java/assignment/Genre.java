cad30f0f4feed5c144605d43fd6ab39926eb93e2
Connor <connor.attard.13@um.edu.mt>
2016-04-25 10:45:30 +0200

    Overhauled genre class

--- src/main/java/assignment/Genre.java
+++ src/main/java/assignment/Genre.java
@@ -9,18 +9,60 @@ package assignment;
  *
  * @author dcata
  */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Genre
 {
-    private enum genre 
+    /* No longer necessary. Genres can be created without having to recompile program
+    public static enum GenreType
     {
-        Fiction, Biography, Comedy, Drama, NonFiction, RealisticFiction, 
-        RomanceNovel, Satire, Tragedy, Tragicomedy, Horror, Other
+        FICTION, BIOGRAPHY, COMEDY, DRAMA, HISTORICAL, 
+        ROMANCE, SATIRE, TRAGEDY, HORROR, CRIME, OTHER
+    }
+    */
+    
+    private static HashMap<String, Genre> handles;
+    
+    private ArrayList<Book> links;
+    private String genreName;
+    private String description;
+    private int count;
+    
+    private Genre(String genreName) {
+        this.genreName = genreName;
+        this.count = 0;
     }
     
-    private genre g;
-    
-    public void setGenre()
-    {
+    public static Genre createGenre(String genreName) {
+        if(handles == null) {
+            handles = new HashMap<String, Genre>();
+        }
         
+        if(handles.containsKey(genreName) == false) {
+            handles.put(genreName, new Genre(genreName));
+            
+            return handles.get(genreName);
+        }
+        else {
+            System.err.println("A genre of this kind has already been created.");
+            
+            return null;
+        }
+    }
+    
+    public void addLink(Book book) {
+        links.add(book);
+        this.count++;
+    }
+    
+    public void removeLink(Book book) {
+        links.remove(book);
+        count--;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
